f7aeec3d043c6a078b449235d99f80892dddfaeb
pedromiguelmvs <pedromiguelmvs@gmail.com>
2021-08-06 05:27:40 -0300

    hotfix: arquivos alterados/deletados para organizar o repositório

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+
+import Home from './pages/home';
+import Panel from './pages/panel';
+import Register from './pages/register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/:name/registrar">
+          <Register />
+        </Route>
+        <Route path="/registros">
+          <Panel />
+        </Route>
+        <Route path="/:name/validar">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
